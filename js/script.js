1faa50d7df7e1b3bf1b79def2bd3775d5ccde7a4
Atul <atul22g2468@gmail.com>
2022-01-31 00:24:49 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let screen = document.getElementById("screen");
+buttons = document.querySelectorAll("button");
+let screenValue = "";
+
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
+
+    if (buttonText == "←") {
+      var value = screenValue;
+      screenValue = value.substr(0, value.length - 1);
+      screen.value = screenValue;
+    } else if (buttonText == "AC") {
+      screenValue = "";
+      screen.value = screenValue;
+    } else if (buttonText == "=") {
+      screen.value = eval(screenValue);
+      screenValue = "";
+    } else {
+      screenValue += buttonText;
+      screen.value = screenValue;
+    }
+  });
+}
